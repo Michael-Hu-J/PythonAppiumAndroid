bc10cf8e985b328c56e0955ba9a75db1b0b22805
hujian <1103986830@qq.com>
2021-12-07 15:29:42 +0800

    update

--- Page/page_login.py
+++ Page/page_login.py
@@ -9,13 +9,13 @@ class Login(Base):
     def correct_login(self):
         """输入正确的手机号和密码登录"""
         # time.sleep(8)
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
-                       text="17771432222", page_description="输入正确手机号")
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
-                       text="qa1234", page_description="输入正确密码")
-        self.click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
-                           page_description="点击登录")
-        avatar = self.wait_element_explicit(
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
+                          text="17771432222", page_description="输入正确手机号")
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
+                          text="qa1234", page_description="输入正确密码")
+        super().click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
+                              page_description="点击登录")
+        avatar = super().wait_element_explicit(
             element_locate="//android.widget.ImageView[@resource-id='zmsoft.rest.phone:id/iv_avatar']",
             page_description="查找头像").is_displayed()
         return avatar
@@ -23,13 +23,13 @@ class Login(Base):
     def err_username(self):
         """输入错误的手机号码和正确的密码登录"""
         # time.sleep(8)
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
-                       text="17771432223", page_description="输入错误手机号")
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
-                       text="qa1234", page_description="输入正确密码")
-        self.click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
-                           page_description="点击登录")
-        warning = self.wait_element_explicit(
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
+                          text="17771432223", page_description="输入错误手机号")
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
+                          text="qa1234", page_description="输入正确密码")
+        super().click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
+                              page_description="点击登录")
+        warning = super().wait_element_explicit(
             element_locate="//android.widget.LinearLayout[@resource-id='zmsoft.rest.phone:id/parentPanel']",
             page_description="查找提示框").is_displayed()
         return warning
@@ -37,13 +37,13 @@ class Login(Base):
     def err_password(self):
         """输入正确的手机号码和错误的密码"""
         # time.sleep(8)
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
-                       text="17771432222", page_description="输入正确手机号")
-        self.send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
-                       text="qa12345", page_description="输入错误密码")
-        self.click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
-                           page_description="点击登录")
-        warning = self.wait_element_explicit(
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etMobile']",
+                          text="17771432222", page_description="输入正确手机号")
+        super().send_text(element_locate="//android.widget.EditText[@resource-id='zmsoft.rest.phone:id/etPassword']",
+                          text="qa12345", page_description="输入错误密码")
+        super().click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
+                              page_description="点击登录")
+        warning = super().wait_element_explicit(
             element_locate="//android.widget.LinearLayout[@resource-id='zmsoft.rest.phone:id/parentPanel']",
             page_description="查找提示框").is_displayed()
         return warning
@@ -51,9 +51,9 @@ class Login(Base):
     def empty_input(self):
         """不输入手机号码和密码登录"""
         # time.sleep(8)
-        self.click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
-                           page_description="点击登录")
-        warning = self.get_text(
+        super().click_element(element_locate="//android.widget.Button[@resource-id='zmsoft.rest.phone:id/btnLogin']",
+                              page_description="点击登录")
+        warning = super().get_text(
             element_locate="//android.widget.TextView[@resource-id='zmsoft.rest.phone:id/tv_message_dialog']",
             page_description="获取为空时提示内容")
         return warning
--- Page/page_mallgoods.py
+++ Page/page_mallgoods.py
@@ -7,28 +7,28 @@ from Basic.base import Base
 
 class MallGoods(Base):
     def add_goods(self):
-        self.swipe_find_element(element_locate="//android.widget.TextView[@text='商品管理']").click()
-        self.click_element(element_locate="//android.widget.TextView[@text='分类管理']", page_description="点击分类管理")
-        self.click_element(element_locate="//android.widget.TextView[@text='商品分类']", page_description="点击添加商品分类")
+        super().swipe_find_element(element_locate="//android.widget.TextView[@text='商品管理']").click()
+        super().click_element(element_locate="//android.widget.TextView[@text='分类管理']", page_description="点击分类管理")
+        super().click_element(element_locate="//android.widget.TextView[@text='商品分类']", page_description="点击添加商品分类")
 
         # 混合定位，两个属性值均属于同一个元素
-        self.find_element_android_uiautomator(
+        super().find_element_android_uiautomator(
             ui_selector='new UiSelector().resourceId("zmsoft.rest.phone:id/txtContent").text("必填")',
             page_description="点击编辑商品分类名称").click()
-        self.find_element_android_uiautomator(
+        super().find_element_android_uiautomator(
             ui_selector='new UiSelector().resourceId("zmsoft.rest.phone:id/txtContent").text("必填")',
             page_description="编辑商品分类名称").send_keys("自动化测试分类")
         # self.send_text(element_locate="//android.widget.EditText[@text='必填']", text="自动化测试分类", page_description="编辑商品分类名称")
 
-        self.click_element(
+        super().click_element(
             element_locate="//android.widget.TextView[@resource-id='zmsoft.rest.phone:id/text_right_str']",
             page_description="点击保存商品分类")
-        self.click_element(
+        super().click_element(
             element_locate="//android.widget.TextView[@resource-id='zmsoft.rest.phone:id/text_left_str']",
             page_description="点击关闭分类管理")
-        self.click_element(element_locate="//android.widget.TextView[@text='商品信息']", page_description="点击商品信息")
-        self.click_element(element_locate="//android.widget.TextView[@text='添加']", page_description="点击添加商品")
-        self.click_element(
+        super().click_element(element_locate="//android.widget.TextView[@text='商品信息']", page_description="点击商品信息")
+        super().click_element(element_locate="//android.widget.TextView[@text='添加']", page_description="点击添加商品")
+        super().click_element(
             element_locate="//android.widget.LinearLayout[@resource-id='zmsoft.rest.phone:id/layoutAddSelf']",
             page_description="点击手动添加")
 
@@ -39,28 +39,28 @@ class MallGoods(Base):
         text = "test"
         while text != "自动化测试分类":
             # 通过商品分类元素定位到同一父级下的兄弟元素zmsoft.rest.phone:id/detail
-            self.find_element_android_uiautomator(
+            super().find_element_android_uiautomator(
                 ui_selector='new UiSelector().text("商品分类").fromParent(resourceId("zmsoft.rest.phone:id/detail"))',
                 page_description="点击商品分类下拉").click()
             # 一次刚好滑动一个分类
-            self.swipe(start_x=0.5, start_y=0.8, end_x=0.5, end_y=0.77, duration=500)
-            self.click_element(
+            super().swipe(start_x=0.5, start_y=0.8, end_x=0.5, end_y=0.77, duration=500)
+            super().click_element(
                 element_locate="//android.widget.TextView[@resource-id='zmsoft.rest.phone:id/btn_confirm']",
                 page_description="点击确认商品分类")
             # 获取商品分类当前已选择的分类名称
-            text = self.find_element_android_uiautomator(
+            text = super().find_element_android_uiautomator(
                 ui_selector='new UiSelector().text("商品分类").fromParent(resourceId("zmsoft.rest.phone:id/detail"))',
                 page_description="点击商品分类下拉").text
 
-        self.find_element_android_uiautomator(
+        super().find_element_android_uiautomator(
             ui_selector='new UiSelector().text("商品名称").fromParent(resourceId("zmsoft.rest.phone:id/detail"))',
             page_description="编辑商品名称").send_keys("自动化测试商品")
-        self.click_element(element_locate="//android.widget.TextView[@text='保存']", page_description="点击保存商品")
+        super().click_element(element_locate="//android.widget.TextView[@text='保存']", page_description="点击保存商品")
 
     def delete_goods(self):
-        self.swipe_find_element(element_locate="//android.widget.TextView[@text='商品管理']").click()
-        self.click_element(element_locate="//android.widget.TextView[@text='商品信息']", page_description="点击商品信息")
-        self.click_element(element_locate="//android.widget.TextView[@text='自动化测试商品']", page_description="点击商品")
+        super().swipe_find_element(element_locate="//android.widget.TextView[@text='商品管理']").click()
+        super().click_element(element_locate="//android.widget.TextView[@text='商品信息']", page_description="点击商品信息")
+        super().click_element(element_locate="//android.widget.TextView[@text='自动化测试商品']", page_description="点击商品")
 
         # 判断是否滑动到底部，删除按钮在底部
         before_swipe = "before"
@@ -68,15 +68,15 @@ class MallGoods(Base):
         while before_swipe != after_swipe:
             """判断是否滑动到屏幕底部"""
             before_swipe = self.driver.page_source
-            self.swipe_up()
+            super().swipe_up()
             after_swipe = self.driver.page_source
 
-        self.click_element(element_locate="//android.widget.TextView[@text='删除']", page_description="点击删除商品")
-        self.click_element(element_locate="//android.widget.Button[@text='确定']", page_description="点击确定删除商品")
-        self.click_element(
+        super().click_element(element_locate="//android.widget.TextView[@text='删除']", page_description="点击删除商品")
+        super().click_element(element_locate="//android.widget.Button[@text='确定']", page_description="点击确定删除商品")
+        super().click_element(
             element_locate="//android.widget.LinearLayout[@resource-id='zmsoft.rest.phone:id/layout_left']",
             page_description="点击返回到商品管理页")
-        self.click_element(element_locate="//android.widget.TextView[@text='分类管理']", page_description="点击分类管理")
-        self.click_element(element_locate="//android.widget.TextView[@text='自动化测试分类']", page_description="点击商品分类")
-        self.click_element(element_locate="//android.widget.TextView[@text='删除']", page_description="点击删除商品分类")
-        self.click_element(element_locate="//android.widget.Button[@text='确定']", page_description="点击确定删除商品分类")
+        super().click_element(element_locate="//android.widget.TextView[@text='分类管理']", page_description="点击分类管理")
+        super().click_element(element_locate="//android.widget.TextView[@text='自动化测试分类']", page_description="点击商品分类")
+        super().click_element(element_locate="//android.widget.TextView[@text='删除']", page_description="点击删除商品分类")
+        super().click_element(element_locate="//android.widget.Button[@text='确定']", page_description="点击确定删除商品分类")
